49f33b5e357baa042facf22daf8988816cd3c430
Guerth Manzala <guerthmanzala@gmail.com>
2025-12-20 08:27:05 +0100

    initial commit

---
+++ apps/web/src/components/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+import { authClient } from "@/lib/auth-client";
+
+import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "./ui/sonner";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+        {children}
+      </ConvexBetterAuthProvider>
+      <Toaster richColors />
+    </ThemeProvider>
+  );
+}
