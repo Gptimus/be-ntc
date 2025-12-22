768ca1c1271b8f7c6f98f8ce2eddd164f08f4c4b
Guerth Manzala <guerthmanzala@gmail.com>
2025-12-22 08:57:37 +0100

    feat: Add initial setup for landing and admin Next.js applications.

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,42 @@
+import { getToken } from "@/lib/auth-server";
+import type { Metadata } from "next";
+
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
+
+import Providers from "@/components/providers";
+import "@be-ntc/ui/globals.css";
+
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "be-ntc",
+  description: "be-ntc",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const token = await getToken();
+
+  return (
+    <html lang="en" className={outfit.variable} suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers initialToken={token}>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ apps/landing/src/components/providers.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+import { ConvexReactClient } from "convex/react";
+
+import { authClient } from "@/lib/auth-client";
+
+import { Toaster } from "@be-ntc/ui/components/sonner";
+import { ThemeProvider } from "./theme-provider";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function Providers({
+  children,
+  initialToken,
+}: {
+  children: React.ReactNode;
+  initialToken?: string | null;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ConvexBetterAuthProvider
+        client={convex}
+        authClient={authClient}
+        initialToken={initialToken}
+      >
+        {children}
+      </ConvexBetterAuthProvider>
+      <Toaster richColors />
+    </ThemeProvider>
+  );
+}
